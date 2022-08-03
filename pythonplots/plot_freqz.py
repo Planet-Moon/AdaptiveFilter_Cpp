835d4fea2c15c70824a153412ee1094154a341a1
Andreas Schroeder <schroeder@mbe-komponenten.de>
2022-08-03 19:05:13 +0200

    add more accurate labels

--- pythonplots/plot_freqz.py
+++ pythonplots/plot_freqz.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 from math import pi as Pi
+from math import log10
 
 def half_vector_size(vector):
     return vector[:int(len(vector)/2)]
@@ -10,6 +11,8 @@ class Freqz:
         self.frequency = frequency
         self.abs = abs
         self.arg = arg
+        self.abs_mode = ''
+        self.frequency_mode = ''
 
     def make_half(self):
         self.frequency = half_vector_size(self.frequency)
@@ -18,7 +21,11 @@ class Freqz:
 
     def normalize_frequency(self):
         self.frequency = [ i/Pi for i in self.frequency ]
+        self.frequency_mode = 'normalized'
 
+    def log_magnitude(self):
+        self.abs = [ 20*log10(x) for x in self.abs ]
+        self.abs_mode = 'log'
 
 class FreqzDecoder(json.JSONDecoder):
     def __init__(self, *args, **kwargs):
@@ -34,7 +41,11 @@ def plot_data(data:Freqz):
     fix, ax = plt.subplots(2, 1, sharex=True)
     ax[0].plot(data.frequency, data.abs)
     ax[0].set_title("Magnitude")
+    if(data.abs_mode == 'log'):
+        ax[0].set_ylabel("dB")
     ax[1].plot(data.frequency, data.arg)
+    if(data.frequency_mode == 'normalized'):
+        ax[1].set_xlabel("Pi")
     ax[1].set_title("Phase")
     plt.show()
 
@@ -42,6 +53,7 @@ def main():
     data = read_json('build/test/freqz_result.json')
     data.make_half()
     data.normalize_frequency()
+    data.log_magnitude()
     plot_data(data)
     pass
 
